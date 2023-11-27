1e46f7eab7e35bc14196699363095f16fe4fe233
sharif-github <107545661+sharif-github@users.noreply.github.com>
2023-11-27 21:28:47 +0600

    demo info added

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById('btn-login').addEventListener('click', function(){
     const emailField = document.getElementById('emailField')
     const passwordField = document.getElementById('passwordField')
     
-    if(emailField.value === 'sharif@gmail.com' && passwordField.value === '1234'){
+    if(emailField.value === 'shariful@gmail.com' && passwordField.value === '1234'){
         location.href='bank.html'
     }
     else{
